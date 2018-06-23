ad45df9bae4f75b23e1b4d5e854f17de2b0893c6
Anatolii Yakushko <shaddyx@gmail.com>
2018-06-23 17:50:18 +0300

    [+] added regexHandlers

--- router.go
+++ router.go
@@ -1,14 +1,18 @@
 package gobot
 
-type RouterHandlerFunc func(event string, msg IncomingChatMessage) *OutgoingChatMessage
+import "regexp"
+
+type RouterHandlerFunc func(msg IncomingChatMessage) *OutgoingChatMessage
 
 type Router struct {
-	handlers map[string]RouterHandlerFunc
+	handlers      map[string]RouterHandlerFunc
+	regexHandlers map[string]RouterHandlerFunc
 }
 
 func NewRouter() Router {
 	return Router{
-		handlers: make(map[string]RouterHandlerFunc),
+		handlers:      make(map[string]RouterHandlerFunc),
+		regexHandlers: make(map[string]RouterHandlerFunc),
 	}
 }
 
@@ -19,13 +23,32 @@ func (r *Router) AddHandler(cmd string, f RouterHandlerFunc) {
 	r.handlers[cmd] = f
 }
 
-func (r *Router) CallHandler(cmd string, msg IncomingChatMessage) *OutgoingChatMessage {
-	if r.handlers[cmd] != nil {
-		return r.handlers[cmd](cmd, msg)
+func (r *Router) AddRegexHandler(cmd string, f RouterHandlerFunc) {
+	if r.regexHandlers[cmd] != nil {
+		panic("Handler already exists:" + cmd)
 	}
+	r.regexHandlers[cmd] = f
+}
+
+func (r *Router) CallHandler(msg IncomingChatMessage) *OutgoingChatMessage {
+	if r.handlers[msg.Text] != nil {
+		return r.handlers[msg.Text](msg)
+	} else {
+		for key, handler := range r.regexHandlers {
+			match, _ := regexp.MatchString(key, msg.Text)
+			if match {
+				handler(msg)
+			}
+		}
+	}
+
 	return nil
 }
 
 func (r *Router) RmHandler(cmd string) {
 	delete(r.handlers, cmd)
+}
+
+func (r *Router) RmRegexHandler(cmd string) {
+	delete(r.regexHandlers, cmd)
 }
--- router_test.go
+++ router_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestRouter_AddHandler(t *testing.T) {
 	called := false
-	f := func(event string, msg IncomingChatMessage) *OutgoingChatMessage {
-		fmt.Println("incoming:" + event)
-		if event != "test" {
+	f := func(msg IncomingChatMessage) *OutgoingChatMessage {
+		fmt.Println("incoming:" + msg.Text)
+		if msg.Text != "test" {
 			t.Error("Error, message must be only test")
 		}
 		called = true
@@ -17,8 +17,39 @@ func TestRouter_AddHandler(t *testing.T) {
 	}
 	router := NewRouter()
 	router.AddHandler("test", f)
-	router.CallHandler("test", IncomingChatMessage{})
-	router.CallHandler("test1", IncomingChatMessage{})
+	router.CallHandler(IncomingChatMessage{
+		Text: "test",
+	})
+	router.CallHandler(IncomingChatMessage{
+		Text: "test1",
+	})
+	if !called {
+		t.Errorf("Not called")
+	}
+}
+
+func TestRouter_AddRegexHandler(t *testing.T) {
+	called := false
+	f := func(msg IncomingChatMessage) *OutgoingChatMessage {
+		fmt.Println("incoming:" + msg.Text)
+		if msg.Text != "test" && msg.Text != "test1" {
+			t.Error("Error, message must be only test")
+		}
+
+		called = true
+		return &OutgoingChatMessage{}
+	}
+	router := NewRouter()
+	router.AddRegexHandler("test", f)
+	router.CallHandler(IncomingChatMessage{
+		Text: "test",
+	})
+	router.CallHandler(IncomingChatMessage{
+		Text: "test1",
+	})
+	router.CallHandler(IncomingChatMessage{
+		Text: "zzzzzzzzzzzz",
+	})
 	if !called {
 		t.Errorf("Not called")
 	}
